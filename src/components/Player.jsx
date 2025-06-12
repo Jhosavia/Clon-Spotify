1881a6ce51acfed7089ad649133a26a61ec3b582
Jhosavia <jhosavia@gmail.com>
2025-06-12 11:18:08 -0400

    Correcion de detalles

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
-const Pause = () => ( 
-<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.
-7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7. 
-7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg> 
+export const Pause = () => ( 
+<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"
+  ><path
+    d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"
+  ></path></svg>
 ) 
-const Play = () => ( 
-<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1. 
-05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg> 
+export const Play = () => ( 
+<svg viewBox="0 0 24 24" class="h-8 w-8" fill="currentColor"
+  ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
 )
 export function Player(){
     const [isPlaying, setIsPlaying] = useState(false)
